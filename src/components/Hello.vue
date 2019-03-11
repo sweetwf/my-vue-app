9da5838ab1c04a9567f779c11e16cb0903220b87
wufan <wufan@xiaomai5.com>
2019-03-11 18:34:02 +0800

    模板语法

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -22,6 +22,23 @@
         <p>{{msg}}</p>
         <button v-on:click="reverseMessage">逆转消息</button>
     </div>
+
+    <!-- 表单输入和应用状态的双向绑定 -->
+    <div class="v-model-module">
+        <p>{{msg}}</p>
+        <input type="text" v-model="msg">
+    </div>
+
+    <!-- v-once 执行一次性的插值，当数据改变，插值处的内容不会改变 -->
+    <div class="v-once-module">
+        <p v-once>{{msg}}</p>
+    </div>
+
+    <!-- v-html 输出真正的html代码 -->
+    <div class="v-html-module">
+        <p>Using mustaches:{{rawHtml}}</p>
+        <p>Using v-html directive <span v-html="rawHtml"></span></p>
+    </div>
 </div>
 </template>
 
@@ -38,7 +55,8 @@ export default {
             {id:1,text:"vue.js"},
             {id:2,text:"typescript"}
         ],
-        msg:"1234567890"
+        msg:"1234567890",
+        rawHtml:'<span style="color:red">This should be red.</span>'
     }
   }, 
   methods:{
